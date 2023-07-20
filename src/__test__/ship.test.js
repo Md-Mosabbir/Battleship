273d23d43afcbbaf7426fb56e01b7f1b12863129
Md-Mosabbir <kmosabbir@gmail.com>
2023-07-20 11:27:35 +0600

    Change name to __test__

---
+++ src/__test__/ship.test.js
@@ -0,0 +1,19 @@
+import Ship from '../components/ship'
+
+//Todo: Test for ship creation
+
+describe('Create a Ship', () => {
+  let submarine
+  beforeAll(() => {
+    submarine = Ship(4)
+  })
+
+  it('Checks all the properties of Ship', () => {
+    expect(submarine).toBeDefined()
+    expect(submarine.isSunk()).toEqual(false)
+    expect(submarine.orientation).toMatch('vertical')
+    expect(submarine.coordinates).toEqual([])
+  })
+
+  it('Check wether ships gets hit', () => {})
+})
