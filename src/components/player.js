ea9da4e132986ef85bb7f21cbddca98f20467e26
Md-Mosabbir <kmosabbir@gmail.com>
2023-07-25 13:11:26 +0600

    Create player and compose Gameboard

---
+++ src/components/player.js
@@ -0,0 +1,25 @@
+function Player(name, gameboard) {
+  let currentPlayer = true
+  const missedArray = []
+  const shipsCreated = gameboard.createShips()
+
+  function gameOver() {
+    if (gameboard.trackShips(shipsCreated)) {
+      return true
+    }
+  }
+  function attack(x, y) {
+    gameboard.recieveAttack(shipsCreated, x, y, missedArray)
+  }
+  function assignShips(index, x, y) {
+    gameboard.assignCoordinates(shipsCreated, index, x, y)
+  }
+
+  function switchTurns() {
+    currentPlayer = !currentPlayer
+  }
+
+  return { switchTurns, attack, assignShips, gameOver }
+}
+
+export default Player
