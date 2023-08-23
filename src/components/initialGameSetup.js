994235cbd81c03db6dbcb1fe10f5fcb204b4fdc2
Md-Mosabbir <kmosabbir@gmail.com>
2023-08-23 16:02:00 +0600

    Add eventlistener to start button

--- src/components/initialGameSetup.js
+++ src/components/initialGameSetup.js
@@ -2,9 +2,10 @@ import createGrid from './grids'
 import displayShips from './shipsUI'
 
 function initialGameSetup(random, heroBoard, heroShips) {
+  let currentSet = 1
   createGrid('playerBoard')
   createGrid('enemyBoard')
-  displayShips(random, 'playerBoard-grid')
+  displayShips(random, 'playerBoard-grid', currentSet)
 
   const playerBoard = document.getElementById('playerBoard-grid')
   const ships = document.querySelectorAll('.ship')
@@ -13,7 +14,7 @@ function initialGameSetup(random, heroBoard, heroShips) {
     if (e.target.classList.contains('ship')) {
       const index = e.target.getAttribute('data-ship-index')
       const changeIt = heroBoard.assignOrientation(random, index)
-      displayShips(changeIt, 'playerBoard-grid')
+      displayShips(changeIt, 'playerBoard-grid', currentSet)
       random = changeIt
     }
   })
@@ -28,7 +29,7 @@ function initialGameSetup(random, heroBoard, heroShips) {
       if (e.target.classList.contains('ship')) {
         const index = e.target.getAttribute('data-ship-index')
         const changeIt = heroBoard.assignOrientation(random, index)
-        displayShips(changeIt, 'playerBoard-grid')
+        displayShips(changeIt, 'playerBoard-grid', currentSet)
         random = changeIt
       }
     })
@@ -58,7 +59,7 @@ function initialGameSetup(random, heroBoard, heroShips) {
       )
       // Assign the new coordinates to the ship
       const updatedShips = heroBoard.assignCoordinates(random, shipIndex, x, y)
-      displayShips(updatedShips, 'playerBoard-grid')
+      displayShips(updatedShips, 'playerBoard-grid', currentSet)
       random = updatedShips
       // Clear the draggedShip reference
       draggedShip = null
@@ -71,10 +72,38 @@ function initialGameSetup(random, heroBoard, heroShips) {
   randomButton.addEventListener('click', () => {
     // Generate new random ship positions and display them
     const newRandom = heroBoard.assignRandomCoordinates(heroShips)
-    displayShips(newRandom, 'playerBoard-grid')
+    displayShips(newRandom, 'playerBoard-grid', currentSet)
 
     // Update the 'random' variable to the new positions
     random = newRandom
+  })
+
+  const appearance = document.getElementById('appearence')
+  appearance.addEventListener('click', () => {
+    if (currentSet === 1) {
+      currentSet = 0
+      displayShips(random, 'playerBoard-grid', currentSet)
+    } else if (currentSet === 0) {
+      currentSet = 1
+      displayShips(random, 'playerBoard-grid', currentSet)
+    }
+  })
+
+  const startButton = document.getElementById('start')
+
+  startButton.addEventListener('click', () => {
+    const playerOne = document.getElementById('Player-One')
+    const playerTwo = document.getElementById('Player-Two')
+    if (playerOne.value !== '' && playerTwo.value !== '') {
+      const player = document.getElementById('playerBoard')
+      const enemy = document.getElementById('enemyBoard')
+      const buttons = document.getElementById('game-function')
+      const nav = document.querySelector('nav')
+      player.textContent = ''
+      enemy.textContent = ''
+      buttons.textContent = ''
+      nav.textContent = ''
+    }
   })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import './style.css'
 import Gameboard from './components/gameboard'
-import Player from './components/player'
+// import Player from './components/player'
 import initialGameSetup from './components/initialGameSetup'
 
 const heroBoard = Gameboard()
-const heroShips = heroBoard.createShips()
-
 const villanBoard = Gameboard()
+
+const heroShips = heroBoard.createShips()
 const villanShips = villanBoard.createShips()
 
-const hero = Player('Mosabbir', heroShips, villanShips, true)
-const villan = Player('Mosarrat', villanShips, heroShips, false)
+// const hero = Player('Mosabbir', heroShips, villanShips, true)
+// const villan = Player('Mosarrat', villanShips, heroShips, false)
 
-let random = heroBoard.assignRandomCoordinates(heroShips)
+let playerRandom = heroBoard.assignRandomCoordinates(heroShips)
+let villanRandom = villanBoard.assignRandomCoordinates(villanShips)
 
-initialGameSetup(random, heroBoard, heroShips)
+initialGameSetup(playerRandom, heroBoard, heroShips)
